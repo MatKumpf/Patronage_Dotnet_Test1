3e336f1d9c8009db46e44794cd120d84cc3a5a4c
MatKumpf <mateusz.kumpf@gmail.com>
2016-12-22 22:45:55 +0100

    Załadowanie projektu do repozytorium. Projekt został ukończony.

---
+++ src/Patronage_Test1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Patronage_Test1
+{
+    public class Program
+    {
+        enum Errors { ErrorAccess, ErrorFound, NoError }
+
+        public static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                Console.Write("Aplikacja została uruchomiona bez parametru. Następuje zakończenie programu.");
+                return;
+            }
+            if (!ValidatePath(args[0]))
+            {
+                Console.Write("Format ścieżki jest niezgodny z wykorzystywanym systemem operacyjnym. Następuje zakończenie programu.");
+                return;
+            }
+
+            string path = args[0];
+            Errors err = ExistPath(ref path);
+            if (err == Errors.ErrorAccess)
+            {
+                Console.Write("Brak uprawnień dostępu do wskazanego katalogu. Następuje zakończenie programu.");
+                return;
+            }
+            else if (err == Errors.ErrorFound)
+            {
+                Console.Write("Wskazana ścieżka nie istnieje. Następuje zakończenie programu.");
+                return;
+            }
+            else
+            {
+                if (path[path.Length - 1] != Path.DirectorySeparatorChar)
+                    path += Path.DirectorySeparatorChar;
+            }
+            FileSystemInfo fsi = new DirectoryInfo(path);
+            Tree tree = new Tree(fsi, ObjectType.Directory);
+            BuildTree(ref tree);
+            tree.PrintTreeData();
+            Console.Write("Aby zakończyć naciśnij dowolny przycisk...");
+            Console.ReadKey();
+        }
+
+        // Funkcja zwraca informację czy skazana ścieżka istnieje
+        private static Errors ExistPath(ref string path)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+                di.GetDirectories();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return Errors.ErrorFound;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Errors.ErrorAccess;
+            }
+            return Errors.NoError;
+        }
+
+        // Tworzenie drzewa
+        private static void BuildTree(ref Tree tree)
+        {
+            DirectoryInfo dir = new DirectoryInfo(tree.Object.FullName);
+            DirectoryInfo[] dirTable = dir.GetDirectories();
+            FileInfo[] fileTable = dir.GetFiles();
+            if (fileTable != null)
+            {
+                foreach (DirectoryInfo di in dirTable)
+                {
+                    Tree tempTree = tree.AddChild(di, ObjectType.Directory);
+                    BuildTree(ref tempTree);
+                }
+                foreach (FileInfo fi in fileTable)
+                {
+                    tree.AddChild(fi, ObjectType.File);
+                }
+            }
+        }
+
+        private static bool ValidatePath(string path)
+        {
+            // Walidacja ścieżki dla systemu Windows
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Wyrażenie regularne uwzględnia tradycyjną ścieżkę (np. C:\) oraz przydział sieciowy lecz lokalizacja podawana jest z nazwą hosta, nie waliduje możliwości podania błędnego adresu IP (np. 256.256.256.256 będzie dla niego poprawnym adresem)
+                Regex reg = new Regex(@"^(([a-zA-Z]:\\)|\\\\)([^\\\/:*?\<>|\r\n]+(\\)?)*$");
+                return reg.IsMatch(path);
+            }
+            // Walidacja ścieżki dla systemu Linux
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Regex reg = new Regex(@"^(\/){1}([^\/\0]+(\/)?)*$");
+                return reg.IsMatch(path);
+            }
+            // Walidacja ścieżki dla systemu OSX
+            else
+            {
+                // Wyrażenie regularne uwzględnia typowy dla systemu Unix format ścieżki gdzie separatorem jest "/", nie uwzględnia tradycyjnej formy z separatorem ":" (wykorzystywanym do wersji OS X 8.1)
+                Regex reg = new Regex(@"^(\/){1}([^\/\0:]+(\/)?)*$");
+                return reg.IsMatch(path);
+            }
+        }
+    }
+}
---
+++ src/Patronage_Test1/Tree.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Patronage_Test1
+{
+    public enum ObjectType
+    {
+        File, Directory
+    };
+
+    public class Tree
+    {
+        private List<Tree> _children = new List<Tree>();
+        private static StringBuilder treeStructure = new StringBuilder("");
+        private static StringBuilder typeStructure = new StringBuilder("");
+        private static StringBuilder timeStructure = new StringBuilder("");
+
+        public FileSystemInfo Object { get; set; }
+        public ObjectType Type { get; set; }
+
+        public List<Tree> Children
+        {
+            get
+            {
+                return _children;
+            }
+        }
+
+        public Tree(FileSystemInfo obj, ObjectType type)
+        {
+            Object = obj;
+            Type = type;
+        }
+
+        public Tree AddChild(FileSystemInfo obj, ObjectType type)
+        {
+            Tree nodeItem = new Tree(obj, type);
+            _children.Add(nodeItem);
+            return nodeItem;
+        }
+
+        public void PrintTreeData()
+        {
+            InitializeData("", true);
+            int measureName = MeasureName();
+            string[] newLineChar = { Environment.NewLine, "\n" };
+            string[] splitTree = treeStructure.ToString().Split(newLineChar, StringSplitOptions.RemoveEmptyEntries);
+            string[] splitType = typeStructure.ToString().Split(newLineChar, StringSplitOptions.RemoveEmptyEntries);
+            string[] splitTime = timeStructure.ToString().Split(newLineChar, StringSplitOptions.RemoveEmptyEntries);
+
+            if (measureName <= 16)
+            {
+                ReplayCharInConsole("=", 54);
+                Console.WriteLine();
+                Console.Write("| Folder/File Name |    Type   |    Creation Time    |");
+                Console.WriteLine();
+                ReplayCharInConsole("=", 54);
+                Console.WriteLine();
+                for (int i = 0; i < splitTime.Length; i++)
+                {
+                    Console.Write("| " + splitTree[i]);
+                    ReplayCharInConsole(" ", 16 - splitTree[i].Length);
+                    Console.Write(" | ");
+                    if (splitType[i] == "File")
+                        Console.Write("  " + splitType[i] + "    | " + splitTime[i] + " |");
+                    else
+                        Console.Write(splitType[i] + " | " + splitTime[i] + " |");
+                    Console.WriteLine();
+                }
+
+            }
+            else
+            {
+                ReplayCharInConsole("=", measureName + 38);
+                Console.WriteLine();
+                Console.Write("|");
+                ;
+                ReplayCharInConsole(" ", Math.Ceiling(((double)measureName - 16) / 2));
+                Console.Write(" Folder/File Name ");
+                ReplayCharInConsole(" ", (measureName - 16) / 2);
+                Console.Write("|    Type   |    Creation Time    |");
+                Console.WriteLine();
+                ReplayCharInConsole("=", measureName + 38);
+                Console.WriteLine();
+                for (int i = 0; i < splitTime.Length; i++)
+                {
+                    Console.Write("| " + splitTree[i]);
+                    ReplayCharInConsole(" ", measureName - splitTree[i].Length);
+                    Console.Write(" | ");
+                    if (splitType[i] == "File")
+                        Console.Write("  " + splitType[i] + "    | " + splitTime[i] + " |");
+                    else
+                        Console.Write(splitType[i] + " | " + splitTime[i] + " |");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private void InitializeData(string indent, bool last)
+        {
+            treeStructure.Append(indent);
+            if (last)
+            {
+                treeStructure.Append("\\-");
+                indent += "  ";
+            }
+            else
+            {
+                treeStructure.Append("|-");
+                indent += "| ";
+            }
+            treeStructure.AppendLine(Object.Name);
+            typeStructure.AppendLine(Type.ToString());
+            timeStructure.AppendLine(Object.CreationTime.ToString());
+
+            for (int i = 0; i < Children.Count; i++)
+            {
+                Children[i].InitializeData(indent, i == Children.Count - 1);
+            }
+        }
+
+        private int MeasureName()
+        {
+            string[] newLineChar = { Environment.NewLine, "\n" };
+            string[] temp = treeStructure.ToString().Split(newLineChar, StringSplitOptions.None);
+            int measure = 0;
+            foreach (string text in temp)
+            {
+                if (text.Length > measure)
+                    measure = text.Length;
+            }
+            return measure;
+        }
+
+        private void ReplayCharInConsole(string character, double replay)
+        {
+            for (int i = 0; i < (int)replay; i++)
+                Console.Write(character);
+        }
+    }
+}
